5c1f093f12d441192ffc33ee66d66da7dc215683
Darryl.Mendonez <darryl.mendonez@Laptop.home>
2020-07-18 13:51:01 -0400

    added ThemeContext and AuthContext

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import BookList from './components/Booklist';
+import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
+import AuthContextProvider from './contexts/AuthContext';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
+      </ThemeContextProvider>
     </div>
   );
 }
